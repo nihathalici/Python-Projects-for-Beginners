6926938ca198d2d9de1f55ebbb4c4d30ec704995
Nihat Halici <nihat.halici@gmail.com>
2021-07-25 08:21:18 -0700

    Create Tic_Tac_Toe.py

---
+++ Tic-Tac-Toe-Game/Tic_Tac_Toe.py
@@ -0,0 +1,31 @@
+# prints the display board
+from IPython.display import clear_output
+
+def display_board(board):
+    pass
+
+def select_marker_input():
+    pass
+    
+def place_marker(board, marker, position):
+    pass
+
+def win_check(board, mark):
+    pass
+    
+import random
+
+def choose_first():
+    pass
+
+def space_check(board, position):
+    pass
+
+def full_board_check(board):
+    pass
+
+def player_choice(board):
+    pass
+
+def replay():
+    pass
