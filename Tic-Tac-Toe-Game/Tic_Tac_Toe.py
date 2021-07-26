95858ac4aabb9c16025a6c4be7d8ae315f3e710b
Nihat Halici <nihat.halici@gmail.com>
2021-07-26 03:48:25 -0700

    Update Tic_Tac_Toe.py

--- Tic-Tac-Toe-Game/Tic_Tac_Toe.py
+++ Tic-Tac-Toe-Game/Tic_Tac_Toe.py
@@ -2,13 +2,39 @@
 from IPython.display import clear_output
 
 def display_board(board):
-    pass
+    # this works in jupyter only!
+    # you can use "print('\n'*100)", to scroll up the previous board
+    clear_output()
 
-def select_marker_input():
-    pass
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
     
+
+# To select the marker
+def select_marker_input():
+    marker = ''
+
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+
+    
+# places the marker at the desired position
 def place_marker(board, marker, position):
-    pass
+    board[position] = marker
 
 def win_check(board, mark):
     pass
